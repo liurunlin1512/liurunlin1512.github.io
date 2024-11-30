6e17d04a635a7a64cf77cc1280d3820dd0dc95f0
liurunlin1512 <3473254882@qq.com>
2024-11-30 10:28:06 +0800

    Add files via upload

---
+++ cet/controller.js
@@ -0,0 +1,221 @@
+UI.form = select('form');
+ select('button#signIn').onclick = function(ev){
+     ev.preventDefault() ;
+     let users = Model.users ;
+     let userName = UI.form.user.value.trim() ;
+     let passWord = UI.form.pass.value.trim() ;
+  if(userName.length > 0 && passWord.length > 0){
+          let user = {
+           'userName': userName ,
+           'passWord': passWord
+         } 
+         let exist = false ;
+         for(let u of users){
+             if(u.userName == userName){
+                 exist = true ;
+                 break ;
+             }
+         }
+         if(exist){
+             UI.footerLog(userName + '已经存在， 注册不成功！') ;
+         }else{
+             UI.footerLog(userName + '注册成功！') ;
+             users.push(user) ;
+         }
+         let s = JSON.stringify(users) ;
+         localStorage.setItem('users', s) ;
+}else{
+  UI.footerLog("无效注册，用户名和密码不能为空");
+ 
+}
+} ; 
+
+ select('button#loginIn').onclick = function(ev){
+     ev.preventDefault() ;
+     let users = Model.users ;
+     let userName = UI.form.user.value ;
+     let passWord = UI.form.pass.value ;
+     
+    let success = false ;
+    for(let u of users){
+      if(u.userName === userName && u.passWord === passWord){
+         success = true ;
+         break ;
+      }
+    }
+    if(success &&  Model.CET6.length > 5000 ){
+      UI.log(userName + '成功登录！') ;
+      Model.user = userName ;
+      UI.form.style.display = 'none' ;
+      let learned = localStorage.getItem( Model.user + '-learned') ;
+      if(learned){
+         Model.learned = JSON.parse(learned) ;
+      }else{
+        Model.learned = [] ;
+        learned = [] ;
+      }
+
+      let learning = [] ;
+      
+           for(let i=0;i < Model.numOfLearning ; i++){
+             let rand = Math.floor(Math.random() * Model.CET6.length ) ;
+             let word = {} ;
+                 word.sn = rand ;
+                 word.level = Model.CET6[rand].level ;
+                 if(learned.length > Model.numOfLearning ){
+                    for(let ld of learned){
+                       if(ld.sn == rand){
+                          word.level = ld.level;
+                          word.timer = ld.timer ? ld.timer : null ;
+                          break;
+                       }
+                    }
+                 }
+             learning.push(word) ;
+            }
+         Model.learning =  learning ;
+       UI.printWord() ;
+       UI.userStatus() ;  
+    }else{
+      if(!success){
+         UI.footerLog(userName + '登录不成功，请查看用户名和密码！') ;
+         }
+      if(Model.CET6.length < 5000){
+         UI.footerLog('单词库还未加载完毕，请等会儿再登录！') ;
+      }
+   }
+ } ;
+
+
+ select('button#firstWord').onclick = function(){
+    Model.pos = 0 ;
+    UI.printWord() ;
+    
+ } 
+
+
+ select('button#nextWord').onclick = function(){
+    if( Model.pos < Model.learning.length -1){
+         Model.pos ++ ;
+     }else{
+         Model.pos = 0 ;
+     }
+        UI.printWord() ;
+                    
+ } 
+
+ select('button#lastWord').onclick = function(){
+     Model.pos = Model.learning.length - 1  ;
+     UI.printWord() ;
+    
+ }
+
+UI.cnDoms = document.querySelectorAll('p.cn') ;
+for(let cn of UI.cnDoms){
+ cn.onclick = function(){
+     let smile = ['😀','😁','😄','😉','😊','😋'] ;
+     let cry = ['😒','😓','😧','😩','😭','😖'] ;
+     let txt = cn.textContent ;
+     let pos = Model.pos ;
+     let currentSn = Model.learning[pos].sn ;
+     if(txt === Model.CET6[currentSn].cn){
+        let s = smile[Math.floor(Math.random()*smile.length)] ;
+        UI.response( s+ "对" + s);
+        Model.learning[pos].level -- ;
+        this.className += ' right' ;
+     }else{
+        let s = cry[Math.floor(Math.random()*smile.length)] ;
+        UI.response( s+ "错" + s);
+        Model.learning[pos].level ++ ;
+        this.className += ' wrong' ;
+     }
+     let timer = new Date() ;
+     let year = timer.getFullYear() % 100 + '' ;
+     let month = timer.getMonth() + 1 ;
+         month > 9 ? month = month :  month = '0' + month  ;
+     let date = timer.getDate() ;
+     Model.learning[pos].timer = year + month + date;
+ }
+}
+
+select('button#saveWord').onclick = function(){
+ if(Model.pos === Model.numOfLearning -1){
+  let learned = Model.learned ;
+  if(learned.length >= Model.numOfLearning){
+       for(let word of Model.learning){
+             let found = false ;
+             for(let l of learned){
+                 if (l.sn == word.sn){
+                     if(word.timer) l.timer = word.timer ;
+                     if(l.level > word.level )  l.level = word.level ;
+                     found = true ;
+                     break ;
+                 }
+             }
+           if(!found){
+             let w = {} ;
+             w.sn = word.sn ; 
+             w.level = word.level ; 
+             if(word.timer) {
+              w.timer = word.timer;
+             }
+             learned.push(w) ;
+           }
+         }
+   }else{
+      learned = [] ;
+      for(let w of Model.learning){
+         let l = {} ;
+         l.sn = w.sn ;
+         l.level = w.level ;
+         learned.push(l) ;
+      }
+     }
+   let str = JSON.stringify(learned) ;
+   localStorage.setItem(Model.user+'-learned', str) ;
+   UI.log("您曾学过单词总计： " + learned.length + " 个！" ) ;
+   UI.userStatus() ;
+ }else{
+   UI.log('本组单词还未背完，不能存储学习进度！') ;
+ }
+};
+
+select('button#reviewWord').onclick = function(){
+
+let learned = Model.learned ;
+if(learned.length >= 2 * Model.numOfLearning){
+Model.learning = [] ;
+let randLearned = function(){
+   let rand = Math.floor(Math.random() * learned.length) ;
+   let word = learned[rand] ;
+   if(word.level < 1 ){
+     randLearned() ;
+   }else{
+     return word ; 
+    }
+ } ;
+for(let i=0;i < Model.numOfLearning  ;i++){
+   let word = randLearned() ;
+   if(word){
+      let en = Model.CET6[word.sn].en ;
+      let pn = Model.CET6[word.sn].pn ;
+      let cn = Model.CET6[word.sn].cn ;
+      word.cn = cn ; word.en = en ; word.pn = pn ;
+      Model.learning.push(word) ;
+   }
+}
+Model.pos = 0 ;
+Model.numOfLearning = Model.learning.length ;
+UI.printWord() ;
+UI.response('复习'+ Model.learning.length +'个单词！');
+}else{
+UI.log('您没背完2组单词，不能进入复习环节！') ;
+}
+} ; 
+
+
+function select(s){
+  let dom = document.querySelector(s) ;
+  return dom ;
+  
+}
---
+++ cet/model.js
@@ -0,0 +1,148 @@
+let Model = { 
+    timerBegin : new Date() ,
+    numOfLearning : 30,
+    learning : [] ,
+    learned : []
+} ;
+    
+    Model.CET6 = [] ;
+{
+let createCET6 = function(s){
+    let cetArr = s.split('\r\n') ;
+ for(let i=0;i<cetArr.length;i++){
+    let wordArr = cetArr[i].split('\t');
+    let obj = {} ;
+      obj.en = wordArr[0] ;
+      obj.pn = wordArr[1] ;
+      obj.cn = wordArr[2] ;
+      let enlength = obj.en.length ;
+      if(enlength > 1 && enlength < 5){
+        obj.level = 1 ;
+      }else{
+        if(enlength < 8 ){
+            obj.level = 2 ;
+        }else{
+            obj.level = 3 ;
+        }
+      }
+      Model.CET6.push(obj) ;
+  }
+}
+let cet6String = "a	/ei/	art.一(个);每一(个);(同类事物中)任一个\r\nabandon	/ə'bændən/	vt.离弃,丢弃;遗弃,抛弃;放弃\r\nabdomen	/æb'dəumen/	n.腹,下腹(胸部到腿部的部分)" ;
+createCET6(cet6String) ;
+fetch('cet/cet1.txt') 
+.then(resp => resp.text())
+.then(txt => {
+    Model.CET6 = [] ;
+    createCET6(txt) ;
+    UI.footerLog('系统成功读取了'+ Model.CET6.length +'个单词！') ;
+}) ;
+setTimeout(function(){ 
+    fetch('cet/cet2.txt') 
+    .then(resp => resp.text())
+    .then(txt => {
+     createCET6(txt) ;
+    UI.footerLog('系统成功读取了'+ Model.CET6.length +'个单词！') ;
+}) ;
+} , 2 * 1000); 
+setTimeout(function(){
+    fetch('cet/cet3.txt')
+    .then(res => res.text())
+    .then(txt => {
+        createCET6(txt) ;
+        UI.footerLog('系统最后成功读取了'+ Model.CET6.length +'个单词！') ;
+    } );
+} , 5 * 1000) ;
+
+}
+Model.pos = 0 ; 
+Model.users = [] ;
+    { 
+        let str = localStorage.getItem('users') ;
+        if(str){
+         let users = JSON.parse(str) ;
+          Model.users = users ;
+        }
+      
+       
+    }
+
+let UI = {} ; 
+UI.printWord = function(){
+        let CET6 = Model.CET6 ;
+        let learning = Model.learning ;
+        let pos = Model.pos ;
+        let currentSn = parseInt(learning[pos].sn) ;
+
+        select('p#en').textContent = CET6[currentSn].en ;
+        select('p#pn').textContent = CET6[currentSn].pn ;
+        let correctCn = CET6[currentSn].cn ;
+        
+        select('span#level').textContent = '难度: ' + learning[pos].level;
+        let cnArr = [] ;
+        
+        let ok = false ; 
+        for(let i=0 ; i < 5 ; i++){
+            let lv = Math.random() * (5 - i) ;
+            if(lv < 1 && !ok ){
+                ok = true ;
+                cnArr.push(correctCn) ;
+            }else{
+                let rand = Math.floor(Math.random() * CET6.length ) ;
+                cnArr.push(CET6[rand].cn) ;
+                }
+         }
+         if(!ok){
+                ok = true ;
+                cnArr[4] = correctCn ;
+            }
+           
+        for(let i=1; i<6 ;i++){
+           select('p#cn'+ i).textContent =  cnArr[i-1] ;
+           select('p#cn'+ i).className = 'cn' ; 
+        }
+    let s = "" ;
+    if (learning[pos].timer){
+      let d = learning[pos].timer ;
+      s = '哟，您在'+ d.slice(0,2) +'年' +  d.slice(2,4)  + '月' + (d.slice(4))+ '日'+' 学过'  ; 
+    }else{
+        s = "哟，您这个单词没学过。"
+    }
+    
+    UI.log(s + '--本组进度@'+ (pos+1)+'/' + Model.numOfLearning  +'.');
+
+  } ;
+
+
+UI.log = function(s){
+select('p#log').textContent = s ;
+};
+UI.footerLog = function(s){
+select('footer').textContent = s ;
+setTimeout(() => {
+    select('footer').textContent = "江西科技师范大学 刘润林 2024--2025" ;
+}, 3*1000);
+};
+UI.response = function(s){
+select('span#response').textContent = s ;
+setTimeout(() => {
+    select('span#response').textContent = '' ;
+}, 2000);
+}
+UI.userStatus = function(){
+let easy = 0, normal = 0, hard=0 ;
+for(let word of Model.learned){
+    if(word.level <= 0){
+        easy ++ ;
+    }else if(word.level < 3){
+        normal ++ ;
+     }else{
+        hard ++ ;
+     }
+}
+select('p#status span#user').textContent = Model.user ;
+select('p#status span#easy').textContent = easy ;
+select('p#status span#normal').textContent = normal ;
+select('p#status span#hard').textContent = hard ;
+
+} ;
